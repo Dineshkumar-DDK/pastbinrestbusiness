e62e1cd97c9c759fba2b8e30b62db76123733744
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2026-01-28 15:32:15 +0530

    .

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 import express from 'express';
+
+
+import { url } from './utils/constants';
 import { healthz } from './routes/health';
 import { createPaste } from './routes/createPaste';
 import { fetchPaste } from './routes/fetchPaste';
 import { viewPaste } from './routes/viewPaste';
 
-
 const app = express();
 app.use(express.json());
 
-app.get('/healthz', healthz);
-app.post('/api/paste', createPaste);
-app.get('/api/paste/:id', fetchPaste);
-app.get('/p/:id', viewPaste);
+app.get(url.HEALTHZ, healthz);
+app.post(url.PASTES, createPaste);
+app.get(url.FETCH_PASTES, fetchPaste);
+app.get(url.VIEW_PASTES, viewPaste);
 
 export default app;
